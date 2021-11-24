70d213f7fbb84659301d0bf7a4f945c757138a8b
Ignacio Wolf <ignaciowolf10@gmail.com>
2021-11-24 15:55:30 -0300

    Eventos

--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import ItemCount from "./ItemCount";
+import {useState} from "react"
+import { Link } from "react-router-dom";
 
 function ItemDetail({item}) {
+    const [compraCant, setCompraCant] = useState(0)
+
+    const onAdd = (cantidad) => {
+        alert("Agregaste " + cantidad + " productos al carrito");
+        setCompraCant(cantidad)
+    }
+
     return (
-        <div>
+        <div className="row justify-content-center text-center g-0">
             <h2>{item.producto}</h2>
             <p>{item.precio}</p>
-            <ItemCount stock={item.stock}/>
+            {
+                compraCant === 0
+                ? <ItemCount stock={item.stock} onAdd={onAdd}/>
+                : <Link to="/cart"><button className="btn btn-info">Ir al carrito</button></Link>
+            }
+            
         </div>
     )
 }
