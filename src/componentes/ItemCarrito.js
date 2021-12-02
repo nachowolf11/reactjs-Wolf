dd22362169d909d555f288607c6cf3b775ab281f
Ignacio Wolf <ignaciowolf10@gmail.com>
2021-12-02 16:32:46 -0300

    borrarItem

--- src/componentes/ItemCarrito.js
+++ src/componentes/ItemCarrito.js
@@ -1,10 +1,11 @@
-const ItemCarrito = ({imagen,producto,cantidad,precio}) => {
+const ItemCarrito = ({imagen,producto,cantidad,precio,children}) => {
     return (
         <div className="border border-secondary p-3">
             <h2>{producto}</h2>
             <div>CANTIDAD: {cantidad}</div>
             <div>PRECIO: {precio}</div>
             <div>SUBTOTAL: {precio*cantidad}</div>
+            {children}
         </div>
     );
 }
